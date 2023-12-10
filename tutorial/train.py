fe66ce58d5a43d9f28ded448fb37f8b54bc21952
Kusal Debnath <debnathk@vcu.edu>
2023-12-10 01:04:13 -0500

    Created model.py, created .gitignore, modified todo, modified train.py

--- tutorial/train.py
+++ tutorial/train.py
@@ -4,6 +4,7 @@ from utils import tokenize, stem, bag_of_words
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
+from model import NeuralNet
 
 with open('tutorial/intents.json', 'r') as f:
     intents = json.load(f)
@@ -40,21 +41,58 @@ for pattern_sentence, tag in xy:
 X_train = np.array(X_train)
 y_train = np.array(y_train)
 
-# Create a dataset class
-def ChatDataSet(Dataset):
+# Hyper-parameters 
+num_epochs = 1000
+batch_size = 8
+learning_rate = 0.001
+input_size = len(X_train[0])
+hidden_size = 8
+output_size = len(tags)
+
+# Create dataset for training
+class ChatDataset(Dataset):
     def __init__(self):
         self.n_samples = len(X_train)
         self.x_data = X_train
         self.y_data = y_train
 
-    def __getitem__(self, idx):
-        return self.x_data[idx], self.y_data[idx]
+    # support indexing such that dataset[i] can be used to get i-th sample
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
 
+    # we can call len(dataset) to return the size
     def __len__(self):
         return self.n_samples
-    
-# Set hyperparameters
-batch_size = 8
 
-dataset = ChatDataSet(X_train, y_train)
-train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+dataset = ChatDataset()
+train_loader = DataLoader(dataset=dataset,
+                          batch_size=batch_size,
+                          shuffle=True,
+                          num_workers=0)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+
+# Loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+for epoch in range(num_epochs):
+    for (words, labels) in train_loader:
+        words = words.to(device)
+        labels = labels.to(device)
+
+        # Forward pass
+        outputs = model(words)
+        loss = criterion(outputs, labels.long())
+
+        # Backward pass and optimizer step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    if (epoch+1) % 100 == 0:
+        print(f"epoch: {epoch+1}/{num_epochs}, loss={loss.item():.4f}")
+
+print(f"Final loss, loss={loss.item():.4f}")
